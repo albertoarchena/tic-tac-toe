64ddd7be9ec4794556a78a366f6408fc914735c2
albertoarchena <albertoarchenaomg@gmail.com>
2023-07-20 20:20:40 +0200

    formatted code

--- src/App.jsx
+++ src/App.jsx
@@ -1,110 +1,99 @@
-import { useState } from 'react'
-import confetti from "canvas-confetti"
+import { useState } from "react";
+import confetti from "canvas-confetti";
 
-import { Square } from './components/Square'
-import { TURNS } from './constants'
-import { WINNER_COMBOS } from './constants'
-import { WinnerModal } from './components/WinnerModal'
+import { Square } from "./components/Square";
+import { TURNS } from "./constants";
+import { WINNER_COMBOS } from "./constants";
+import { WinnerModal } from "./components/WinnerModal";
 
 function App() {
   const [board, setBoard] = useState(() => {
-     const boardFromStorage = window.localStorage.getItem('board')
-     if(boardFromStorage)  return JSON.parse(boardFromStorage) 
-     return Array(9).fill(null)
-  })
-  
-  const[turn, setTurn] = useState(() => {
-    const turnFromStorage = window.localStorage.getItem('turn')
-    return turnFromStorage ?? TURNS.X
-  })
+    const boardFromStorage = window.localStorage.getItem("board");
+    if (boardFromStorage) return JSON.parse(boardFromStorage);
+    return Array(9).fill(null);
+  });
+
+  const [turn, setTurn] = useState(() => {
+    const turnFromStorage = window.localStorage.getItem("turn");
+    return turnFromStorage ?? TURNS.X;
+  });
   // null es que no hay ganador y false es que hay empate
-  const[winner, setWinner] = useState(null)
+  const [winner, setWinner] = useState(null);
 
   const checkWinner = (boardToCheck) => {
     // revisamos todas las combinaciones ganadoras
-    for(const combo of WINNER_COMBOS){
-      const [a, b, c] = combo
+    for (const combo of WINNER_COMBOS) {
+      const [a, b, c] = combo;
       if (
         boardToCheck[a] &&
         boardToCheck[a] === boardToCheck[b] &&
-        boardToCheck[a] === boardToCheck[c]  
-      ){
-        return boardToCheck[a]
+        boardToCheck[a] === boardToCheck[c]
+      ) {
+        return boardToCheck[a];
       }
     }
     // si no hay ganador
-    return null
-  }
+    return null;
+  };
 
   const resetGame = () => {
-    setBoard(Array(9).fill(null))
-    setTurn(TURNS.X)
-    setWinner(null)
+    setBoard(Array(9).fill(null));
+    setTurn(TURNS.X);
+    setWinner(null);
 
-    window.localStorage.removeItem('board')
-    window.localStorage.removeItem('turn')
-  }
+    window.localStorage.removeItem("board");
+    window.localStorage.removeItem("turn");
+  };
 
   const checkEndGame = (newBoard) => {
-    return newBoard.every((square) => square != null)
-  }
+    return newBoard.every((square) => square != null);
+  };
 
   const updateBoard = (index) => {
     // si ya tiene algo no actualizaremos la posicion
-    if (board[index] || winner) return
+    if (board[index] || winner) return;
     // actualizamos el tablero
-    const newBoard = [... board]
-    newBoard[index] = turn
-    setBoard(newBoard)
+    const newBoard = [...board];
+    newBoard[index] = turn;
+    setBoard(newBoard);
     // cambiamos de turno
-    const newTurn = turn === TURNS.X ? TURNS.O : TURNS.X
-    setTurn(newTurn)
+    const newTurn = turn === TURNS.X ? TURNS.O : TURNS.X;
+    setTurn(newTurn);
     // guardar aqui partida
-    window.localStorage.setItem('board', JSON.stringify(newBoard))
-    window.localStorage.setItem('turn', newTurn)
+    window.localStorage.setItem("board", JSON.stringify(newBoard));
+    window.localStorage.setItem("turn", newTurn);
     // revisar si hay ganador
-    const newWinner = checkWinner(newBoard)
+    const newWinner = checkWinner(newBoard);
     if (newWinner) {
-      confetti()
-      setWinner(newWinner)
-    } else if (checkEndGame(newBoard)){
-      setWinner(false)
+      confetti();
+      setWinner(newWinner);
+    } else if (checkEndGame(newBoard)) {
+      setWinner(false);
     }
-  }
-
+  };
 
   return (
-    <main className='board'>
+    <main className="board">
       <h1>Tic Tac Toe</h1>
       <button onClick={resetGame}>Reset</button>
-      <section className='game'>
-        {
-          board.map((square, index) => {
-            return (
-              <Square
-                key={index}
-                index = {index}
-                updateBoard={updateBoard}
-              >
-                {square}
-              </Square>
-            )
-          })
-        }
+      <section className="game">
+        {board.map((square, index) => {
+          return (
+            <Square key={index} index={index} updateBoard={updateBoard}>
+              {square}
+            </Square>
+          );
+        })}
       </section>
 
-      <section className='turn'>
-        <Square isSelected={turn === TURNS.X}>
-          {TURNS.X}
-        </Square>
-        <Square isSelected={turn === TURNS.O}>
-          {TURNS.O}
-        </Square>
+      <section className="turn">
+        <Square isSelected={turn === TURNS.X}>{TURNS.X}</Square>
+        <Square isSelected={turn === TURNS.O}>{TURNS.O}</Square>
       </section>
-      
+
       <WinnerModal resetGame={resetGame} winner={winner} />
-    </main>   
-  )
+    </main>
+  );
 }
 
-export default App
+export default App;
--- src/components/WinnerModal.jsx
+++ src/components/WinnerModal.jsx
@@ -1,22 +1,19 @@
 import { Square } from "./Square";
-export function WinnerModal({winner, resetGame}) {
-    if (winner == null) return null
+export function WinnerModal({ winner, resetGame }) {
+  if (winner == null) return null;
 
-    const winnerText = winner === false ? 'Empate' : 'Ha ganado';
+  const winnerText = winner === false ? "Empate" : "Ha ganado";
 
-    return(       
-      <section className='winner'>
-        <div className='text'>
-          <h2>{winnerText}</h2>
-          <header className='win'>
-            {winner && <Square>{winner}</Square>}
-          </header>
-            
-          <footer>
-            <button onClick={resetGame}>Empezar de nuevo</button>
-          </footer>
-          
-        </div>
-      </section>
-    )
+  return (
+    <section className="winner">
+      <div className="text">
+        <h2>{winnerText}</h2>
+        <header className="win">{winner && <Square>{winner}</Square>}</header>
+
+        <footer>
+          <button onClick={resetGame}>Empezar de nuevo</button>
+        </footer>
+      </div>
+    </section>
+  );
 }
